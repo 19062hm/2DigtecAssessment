0c76f2171a701cd744395c80aa2819b4e3aa3148
Hermione Meadows <50893455+19062hm@users.noreply.github.com>
2022-11-02 11:09:20 +0000

    4 More Trees Added, Array Order Formatted As A List

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ while (ordering.toLowerCase() == "yes") {
   if (menu.toLowerCase() == "p") {
     var pine = prompt("Pine Trees: $8.50 \nHow many Pine Trees would you like to order? (Max 5)");
     if (pine > "5") {
-      alert("Sorry, you can only buy up to five birch trees per order");
+      alert("Sorry, you can only buy up to five pine trees per order");
       pine = prompt("Pine Trees: $8.50 \nHow many Pine Trees would you like to order? (Max 5)");
     } else if (pine <= "0") {
       
@@ -67,8 +67,72 @@ while (ordering.toLowerCase() == "yes") {
       menu = prompt("What would you like to order? Type O for Oak, B for Birch, P for Pine, S for Spruce, K for King Palm, E for Elm, W for Willow, Bon for Bonsai, Acer for Acer Palmatum, Pong for Ponga, Rain for Rainbow Eucalytpus or Kaka for Kakabeak");
     }
   }
+/* Spruce Tree Code */
+  if (menu.toLowerCase() == "s") {
+    var spruce = prompt("Spruce Trees: $8.50 \nHow many Spruce Trees would you like to order? (Max 5)");
+    if (spruce > "5") {
+      alert("Sorry, you can only buy up to five spruce trees per order");
+      spruce = prompt("Spruce Trees: $8.50 \nHow many Spruce Trees would you like to order? (Max 5)");
+    } else if (spruce <= "0") {
+      
+    } else {
+      cost = cost + (8.50 * spruce);
+      order.push(spruce + " Spruce Trees");
+      var again = prompt("Thank you for ordering " + spruce + " spruce trees, would you like to order more trees?")
+    } if (again.toLowerCase() == "yes") {
+      menu = prompt("What would you like to order? Type O for Oak, B for Birch, P for Pine, S for Spruce, K for King Palm, E for Elm, W for Willow, Bon for Bonsai, Acer for Acer Palmatum, Pong for Ponga, Rain for Rainbow Eucalytpus or Kaka for Kakabeak");
+    }
+  }
+/* King Palm Tree Code */
+  if (menu.toLowerCase() == "k") {
+    var kingPalm = prompt("King Palm Trees: $8.50 \nHow many King Palm Trees would you like to order? (Max 5)");
+    if (kingPalm > "5") {
+      alert("Sorry, you can only buy up to five king palm trees per order");
+      kingPalm = prompt("King Palm Trees: $8.50 \nHow many King Palm Trees would you like to order? (Max 5)");
+    } else if (kingPalm <= "0") {
+      
+    } else {
+      cost = cost + (8.50 * kingPalm);
+      order.push(kingPalm + " King Palm Trees");
+      var again = prompt("Thank you for ordering " + kingPalm + " king palm trees, would you like to order more trees?")
+    } if (again.toLowerCase() == "yes") {
+      menu = prompt("What would you like to order? Type O for Oak, B for Birch, P for Pine, S for Spruce, K for King Palm, E for Elm, W for Willow, Bon for Bonsai, Acer for Acer Palmatum, Pong for Ponga, Rain for Rainbow Eucalytpus or Kaka for Kakabeak");
+    }
+  }
+/* Elm Tree Code */
+  if (menu.toLowerCase() == "e") {
+    var elm = prompt("Elm Trees: $8.50 \nHow many Elm Trees would you like to order? (Max 5)");
+    if (elm > "5") {
+      alert("Sorry, you can only buy up to five elm trees per order");
+      elm = prompt("Elm Trees: $8.50 \nHow many Elm Trees would you like to order? (Max 5)");
+    } else if (elm <= "0") {
+      
+    } else {
+      cost = cost + (8.50 * elm);
+      order.push(elm + " Elm Trees");
+      var again = prompt("Thank you for ordering " + elm + " elm trees, would you like to order more trees?")
+    } if (again.toLowerCase() == "yes") {
+      menu = prompt("What would you like to order? Type O for Oak, B for Birch, P for Pine, S for Spruce, K for King Palm, E for Elm, W for Willow, Bon for Bonsai, Acer for Acer Palmatum, Pong for Ponga, Rain for Rainbow Eucalytpus or Kaka for Kakabeak");
+    }
+  }
+/* Willow Tree Code */
+  if (menu.toLowerCase() == "w") {
+    var willow = prompt("Willow Trees: $8.50 \nHow many Willow Trees would you like to order? (Max 5)");
+    if (willow > "5") {
+      alert("Sorry, you can only buy up to five willow trees per order");
+      willow = prompt("Willow Trees: $8.50 \nHow many Willow Trees would you like to order? (Max 5)");
+    } else if (willow <= "0") {
+      
+    } else {
+      cost = cost + (8.50 * willow);
+      order.push(willow + " Willow Trees");
+      var again = prompt("Thank you for ordering " + willow + " willow trees, would you like to order more trees?")
+    } if (again.toLowerCase() == "yes") {
+      menu = prompt("What would you like to order? Type O for Oak, B for Birch, P for Pine, S for Spruce, K for King Palm, E for Elm, W for Willow, Bon for Bonsai, Acer for Acer Palmatum, Pong for Ponga, Rain for Rainbow Eucalytpus or Kaka for Kakabeak");
+    }
+  }
   order.sort()
-  alert("Your order is: \n" + order + "\nTotal Cost: \n$" + cost);
+  alert("Your order is: \n" + order[0] + "\n" + order[1] + "\nTotal Cost: \n$" + cost);
   var ordering = prompt("Type yes if you'd like to place another order");
 }
 
